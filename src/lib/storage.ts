893655655dbb128d6ae949d6fa96b47df271b3e4
LiamDoult <3579535@myuwc.ac.za>
2021-01-02 21:41:14 +0100

    Library Box get
    Get box by box id. This is the client side function for retrieving a
    box.

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -44,4 +44,12 @@ export namespace Box {
     if (res.status === 200) return res.json();
     throw new Error("Unknown issue raise by the server");
   }
+
+  export async function get(_id: string): Promise<Registered> {
+    const res = await fetch(`${url}/storage/box?id=${_id}`, {
+      method: "get",
+    });
+    if (res.status === 200) return res.json();
+    throw new Error("Unknown issue raise by the server");
+  }
 }
